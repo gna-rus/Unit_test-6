58c90f2c845974dcf0a1e6454ca098291a929a0d
gnarus-tm <71601039+gna-rus@users.noreply.github.com>
2023-10-02 12:43:40 +0300

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import AverageValueInList
-
-list1 = [21, 2201, 12]
-list2 = [111, 1111, 1113]
-test = AverageValueInList.AverageValueInList(list1, list2)
-print(test.AverageList1())
-print(test.CompareTwoList())
